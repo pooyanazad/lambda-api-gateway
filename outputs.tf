afd2eb3ee512902b7c4b30fbbb0d5aa456999eb7
Pooyan <pooyan.azadparvar@gmail.com>
2025-02-11 13:19:59 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,4 @@
-output "vpc_id" {
-  description = "The ID of the new VPC"
-  value       = aws_vpc.main.id
-}
-
-output "public_subnet_id" {
-  description = "The ID of the public subnet"
-  value       = aws_subnet.public_subnet.id
-}
-
-output "private_subnet_id" {
-  description = "The ID of the private subnet"
-  value       = aws_subnet.private_subnet.id
+output "api_invoke_url" {
+  description = "API Gateway invoke URL"
+  value       = "${aws_api_gateway_rest_api.hello_api.execution_arn}/deployments/${aws_api_gateway_deployment.hello_api_deployment.id}"
 }
