2575c31d1f4657303b81c82c5b9ef67b98282ed9
Pooyan <pooyan.azadparvar@gmail.com>
2025-02-11 13:19:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,53 +12,94 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  tags = {
-    Name = "BasicVPC"
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_exec_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+}
+
+data "aws_iam_policy_document" "lambda_trust" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "BasicIGW"
-  }
+resource "aws_iam_role_policy_attachment" "lambda_basic_exec" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block             = var.public_subnet_cidr
-  availability_zone       = var.availability_zone
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "PublicSubnet"
-  }
+resource "aws_lambda_function" "hello_lambda" {
+  function_name    = "hello-lambda"
+  role            = aws_iam_role.lambda_role.arn
+  handler         = "lambda_function.handler"
+  runtime         = "python3.9"
+  filename        = "lambda_function.zip"
+  source_code_hash = filebase64sha256("lambda_function.zip")
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidr
-  availability_zone = var.availability_zone
-  tags = {
-    Name = "PrivateSubnet"
-  }
+resource "aws_api_gateway_rest_api" "hello_api" {
+  name        = "hello-api"
+  description = "API Gateway for Hello Lambda"
 }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "PublicRouteTable"
-  }
+resource "aws_api_gateway_resource" "hello_resource" {
+  rest_api_id = aws_api_gateway_rest_api.hello_api.id
+  parent_id   = aws_api_gateway_rest_api.hello_api.root_resource_id
+  path_part   = "hello"
 }
 
-resource "aws_route" "default_route" {
-  route_table_id         = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.hello_api.id
+  resource_id   = aws_api_gateway_resource.hello_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
 }
 
-resource "aws_main_route_table_association" "vpc_rt_association" {
-  vpc_id         = aws_vpc.main.id
-  route_table_id = aws_route_table.public_rt.id
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.hello_api.id
+  resource_id             = aws_api_gateway_resource.hello_resource.id
+  http_method             = aws_api_gateway_method.get_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = aws_lambda_function.hello_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.hello_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_integration_response" "int_response" {
+  rest_api_id = aws_api_gateway_rest_api.hello_api.id
+  resource_id = aws_api_gateway_resource.hello_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_method_response" "method_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.hello_api.id
+  resource_id = aws_api_gateway_resource.hello_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_deployment" "hello_api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration
+  ]
+  rest_api_id = aws_api_gateway_rest_api.hello_api.id
+  stage_name  = "dev"
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
 }
